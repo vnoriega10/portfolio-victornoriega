e6f94f750fd2f37ec9009e5657504d5add745fd4
Victor_Noriega <alfonsomedina0645@gmail.com>
2023-11-19 16:55:57 -0500

    actualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  root: "./"
 });
